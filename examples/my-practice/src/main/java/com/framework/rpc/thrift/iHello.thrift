8a86af5c9899db3e15315373d335f3508b83e08f
tidantian <14231761@qq.com>
2015-09-25 17:16:44 +0800

    update project name

--- examples/my-practice/src/main/java/com/framework/rpc/thrift/iHello.thrift
+++
@@ -1,8 +0,0 @@
-namespace cpp zam.thrift.test  
-namespace py thriftTest  
-namespace java com.zam.thrift.test  
-namespace php thriftTest  
-  
-service Hello {    
-    string helloString(1:string word)    
-}  
